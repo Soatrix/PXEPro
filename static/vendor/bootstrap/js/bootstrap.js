0d3990e9d8c1ee6c00b02086658e6d825643cf35
Dan Alexander <admin@randamonium.co.nz>
2024-10-23 02:17:07 +0000

    Release 0.0.2-DEV

--- static/vendor/bootstrap/js/bootstrap.js
+++ static/vendor/bootstrap/js/bootstrap.js
@@ -135,9 +135,6 @@
         selector = hrefAttr && hrefAttr !== '#' ? hrefAttr.trim() : '';
       }
 
-      // Sanitize the selector to prevent XSS
-      selector = CSS.escape(selector);
-
       try {
         return document.querySelector(selector) ? selector : null;
       } catch (_) {
@@ -1117,7 +1114,7 @@
         return;
       }
 
-      var target = $__default['default'](document).find(selector)[0];
+      var target = $__default['default'](selector)[0];
 
       if (!target || !$__default['default'](target).hasClass(CLASS_NAME_CAROUSEL)) {
         return;
